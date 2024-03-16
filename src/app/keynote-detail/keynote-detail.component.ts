22d0c5c5573042cedf2b2e4700bf3a06ddddd6cf
angelmaria <angelmaria75@gmail.com>
2024-03-16 13:58:38 +0100

    añadiendo botones de ver, editar y borrar en el listado de keynotes. Funcionalidad solo en ver

--- src/app/keynote-detail/keynote-detail.component.ts
+++ src/app/keynote-detail/keynote-detail.component.ts
@@ -1,16 +1,17 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Keynote } from '../models/keynote.model';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-keynote-detail',
   standalone: true,
-  imports: [HttpClientModule],
+  imports: [HttpClientModule, RouterLink],
   templateUrl: './keynote-detail.component.html',
   styleUrl: './keynote-detail.component.css'
 })
 export class KeynoteDetailComponent implements OnInit {
+
   keynote: Keynote | undefined;
 
   constructor(
@@ -29,6 +30,20 @@ export class KeynoteDetailComponent implements OnInit {
         this.keynote = keynoteBackend;
         console.log(this.keynote);
       });
+
+      // a mayores, se podría llamar a otros controladores y traer más datos
+
+      // traer todos los capítulos de un libro
+      // /chapters/filter-by-book/id
+
+      // traer todos los comments de un libro
+      // /comments/filter-by-book/id 
+
+      // traer todas las categorías de un libro
+      // /categories/filter-by-book/id
+
+      // traer el autor del libro
+      // /author/fiter-by-book/id
 
     });
 
--- src/app/keynote-list/keynote-list.component.ts
+++ src/app/keynote-list/keynote-list.component.ts
@@ -2,26 +2,40 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Keynote } from '../models/keynote.model';
+import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-keynote-list',
   standalone: true,
-  imports: [RouterLink, HttpClientModule],
+  imports: [RouterLink, HttpClientModule, NgbAlertModule, DatePipe],
   templateUrl: './keynote-list.component.html',
   styleUrl: './keynote-list.component.css'
 })
 export class KeynoteListComponent implements OnInit {
+  
   keynotes: Keynote[] = [];
+  showDeleteKeynoteMessage: boolean = false
 
-  constructor(private http: HttpClient) {}
+  constructor(private httpClient: HttpClient) {}
 
   ngOnInit(): void {
+    this.loadsKeynotes();
+  }
+
+  delete(keynote: Keynote) {
     // traer una lista de keynotes del backend: crea y ejecuta una petición HTTP contra un controlador Backend
-    const backenUrl = 'http://localhost:8080/keynotes';
-    this.http.get<Keynote[]>(backenUrl).subscribe(keynotes => {
-      console.log(keynotes);
-      // guardamos la respuesta del backend en una variable para poder usarla
-      this.keynotes = keynotes;
-    });
+    const url = 'http://localhost:8080/keynotes';
+    this.httpClient.delete(url).subscribe(keynotes => {
+      this.loadsKeynotes();
+      this.showDeleteKeynoteMessage = true;
+    }); // recarga los keynoets después de borrar
+  }
+  hideDeleteKeynoteMessage() {
+    this.showDeleteKeynoteMessage = false;
+  }
+  private loadsKeynotes() {
+    const url = 'http://localhost:8080/keynotes';
+    this.httpClient.get<Keynote[]>(url).subscribe(keynotes => this.keynotes = keynotes);
   }
 }
