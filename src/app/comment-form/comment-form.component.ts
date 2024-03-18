addc0063e9b4b878bdb858b716a51d48693e91b9
Francisco Javier Baeza-Rojano Ossorio <148579168+JavierBRO@users.noreply.github.com>
2024-03-18 13:28:41 +0100

    componentes list comments y form.TS

--- src/app/comment-form/comment-form.component.ts
+++ src/app/comment-form/comment-form.component.ts
@@ -1,6 +1,10 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { Component } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { User } from '../models/user.model';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Keynote } from '../models/keynote.model';
+import { CommentModel } from '../models/commentmodel.model';
 
 @Component({
   selector: 'app-comment-form',
@@ -9,41 +13,79 @@ import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
   templateUrl: './comment-form.component.html',
   styleUrl: './comment-form.component.css'
 })
-export class CommentFormComponent {
+export class CommentFormComponent implements OnInit {
 
-  commentForm = this.fb.group({
-    id: [0],
-    rating: [0.0],
-    opinion: [''],
-    //user: [],
-    //keynote: [[]]
+  commentForm = new FormGroup({
+    id: new FormControl<number>(0),
+    rating: new FormControl<number>(0),
+    opinion: new FormControl<string>(''),
+    user: new FormControl(),
+    keynote: new FormControl()
+
   });
 
-  constructor (private fb: FormBuilder, private httpCliente: HttpClient) {}
+  isUpdate: boolean = false;
+  users: User[] = [];
+  keynote: Keynote[] = [];
 
-  save() {
-    // Extraer los valores de cada input escritos por el usuario
-    console.log("Guardando comment");
-    const id = this.commentForm.get('id')?.value ?? 0;
-    const rating = this.commentForm.get('rating')?.value ?? 0.0;
-    const opinion = this.commentForm.get('opinion')?.value ?? 'opinion por defecto';
-    const user = this.commentForm.get('user')?.value ?? 'user1';
-    const keynote = this.commentForm.get('keynote')?.value ?? [];
+  constructor(private fb: FormBuilder,
+    private httpClient: HttpClient,
+    private router: Router,
+    private activatedRoute: ActivatedRoute) {
 
-    // Crear el objeto utilizando los valores extraídos
-    /*
-    const commentToSave: Comment = {
-      id: id,
-      rating: rating,
-      opinion: opinion,
-      user: user,
-      keynote: keynote
-    }
-    */
-    //console.log(commentToSave);
-    // Enviar el objeto a backend utilizando HttpCliente
-    const url = 'http://localhost:8080/comments';
-    //this.httpCliente.post<Comment>(url, commentToSave).subscribe (comment => console.log(comment));
   }
 
+  ngOnInit(): void {
+
+    this.httpClient.get<User[]>('http://localhost:8080/users')
+      .subscribe(users => this.users = users);
+
+    this.httpClient.get<Keynote[]>('http://localhost:8080/keynotes')
+      .subscribe(keynotes => this.keynotes = keynotes);
+
+    this.activatedRoute.params.subscribe(params => {
+      const id = params['id'];
+      if (!id) return;
+
+      this.httpClient.get<CommentModel>('http://localhost:8080/comments/' + id)
+        .subscribe(commentFromBackend => {
+          // cargar el comentario obtenido en el formulario commentForm
+          this.commentForm.reset({
+            id: commentFromBackend.id,
+            rating: commentFromBackend.rating,
+            opinion: commentFromBackend.opinion,
+            user: commentFromBackend.user, // carga un user en el selector
+            keynote: commentFromBackend.keynote
+          });
+
+          this.isUpdate = true;
+
+        });
+    });
+  }
+  save() {
+    const comment: CommentModel = this.commentForm.value as CommentModel;
+      console.log(comment);
+
+      if (this.isUpdate) {
+        const url = 'http://localhost:8080/comments/' + comment.id;
+        this.httpClient.put<CommentModel>(url, comment).subscribe(commentFromBackend => {
+          this.router.navigate(['/comments', commentFromBackend.id, 'detail']);
+        });
+
+      } else {
+        const url = 'http://localhost:8080/comments';
+        this.httpClient.post<CommentModel>(url, comment).subscribe(commentFromBackend => {
+          this.router.navigate(['/comments', commentFromBackend.id, 'detail']);
+        });
+      }
+  }
+  compareObjects(o1: any, o2: any): boolean {
+    // console.log("Comparando objetos: ", o1, o2);
+
+    if(o1 && o2) {
+      return o1.id === o2.id;
+    }
+    return o1 === o2;
+  }
 }
--- src/app/comment-list/comment-list.component.ts
+++ src/app/comment-list/comment-list.component.ts
@@ -2,32 +2,45 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { CommentModel } from '../models/commentmodel.model';
+import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 @Component({
   selector: 'app-comment-list',
   standalone: true,
-  imports: [RouterLink, HttpClientModule],
+  imports: [RouterLink, HttpClientModule, NgbAlertModule],
   templateUrl: './comment-list.component.html',
   styleUrl: './comment-list.component.css'
 })
 export class CommentListComponent implements OnInit {
 
   comments: CommentModel[] = [];
+  showDeletedCommentMessage: boolean = false;
 
   constructor(private http: HttpClient) {}
     
   ngOnInit(): void {
 
-        // traer una lista de comments del backend: crea y ejecuta una petición HTTP contra un controlador Backend
-    const backenUrl = 'http://localhost:8080/comments';
-    this.http.get<CommentModel[]>(backenUrl).subscribe(commentsBackend => {
-      // guardamos la respuesta del backend en una variable para poder usarla
-      this.comments = commentsBackend;
-    });
+    this.loadComments();
 
   }
-
+  delete(comment: CommentModel) {
+    const url = 'http://localhost:8080/comments/' + comment.id;
+    this.http.delete(url).subscribe(Response => {
+      this.loadComments();
+      this.showDeletedCommentMessage = true;
+    });
+  }
+  hideDeletedCommentMessage() {
+    this.showDeletedCommentMessage = false;
+  }
+  
+  private loadComments() {
+    const backenUrl = 'http://localhost:8080/comments';
+    this.http.get<CommentModel[]>(backenUrl).subscribe(commentsBackend => {
+      this.comments = commentsBackend;
+    });
+  }
 }
 
     
