ce4c72b4463ae6616d29402cd50e59fffa9c9661
Johanamacias <148787180+Johanamacias@users.noreply.github.com>
2024-03-18 17:07:13 +0100

    fixed app routes para about

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -183,6 +183,7 @@ export const routes: Routes = [
         path: 'about',
         component: AboutUsComponent
     },
+    {
         path: 'rooms/:id/update',
         component: RoomFormComponent
     },
