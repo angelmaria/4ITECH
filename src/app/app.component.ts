0c52a7929c170fbf5f5332c5da508f8998b07d7f
Francisco Javier Baeza-Rojano Ossorio <148579168+JavierBRO@users.noreply.github.com>
2024-03-19 11:57:03 +0100

    cambios favicon y titulo pestaña

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,5 +11,5 @@ import { FooterComponent } from './footer/footer.component';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-4iTech';
+  title = '4iTech';
 }
