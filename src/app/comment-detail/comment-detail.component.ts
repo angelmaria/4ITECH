a9765932d6fc7c92d205e202accb42fc1cf95871
Francisco Javier Baeza-Rojano Ossorio <148579168+JavierBRO@users.noreply.github.com>
2024-04-01 20:31:41 +0200

    Rating estrellitas en comments detail HTML

--- src/app/comment-detail/comment-detail.component.ts
+++ src/app/comment-detail/comment-detail.component.ts
@@ -2,19 +2,21 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { CommentModel } from '../models/commentmodel.model';
+import { NgbRatingModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 @Component({
   selector: 'app-comment-detail',
   standalone: true,
-  imports: [HttpClientModule, RouterLink],
+  imports: [HttpClientModule, RouterLink, NgbRatingModule],
   templateUrl: './comment-detail.component.html',
   styleUrl: './comment-detail.component.css'
 })
 export class CommentDetailComponent implements OnInit{
 
   comment: CommentModel | undefined;
-
+  rating = 0;
+commentForm: any;
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -31,6 +33,7 @@ export class CommentDetailComponent implements OnInit{
       const backendUrl = 'http://localhost:8080/comments/' + id;
       this.http.get<CommentModel>(backendUrl).subscribe(commentBackend => {
         this.comment = commentBackend;
+        
         // console.log(this.comment);
       });
     });
