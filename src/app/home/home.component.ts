2e34870f2a2dc146e643f6cb3e02482834d2fdc8
Angel Martinez <angelmaria75@gmail.com>
2024-04-02 01:01:38 +0200

    cambios en la home: añado video y carusel dinámico con keynotes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { NgbCarouselConfig, NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+import { Keynote } from '../models/keynote.model';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
+export class NgbdCarouselConfig {
+
+	constructor(config: NgbCarouselConfig) {
+		// customize default values of carousels used by this component tree
+		config.interval = 20000;
+		config.wrap = false;
+		config.keyboard = false;
+		config.pauseOnHover = false;
+	}
+}
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NgbCarouselModule, RouterLink, HttpClientModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  constructor(private httpClient: HttpClient) {}
+
+  keynotes: Keynote[] = [];
+
+ngOnInit(): void {
+  this.loadsKeynotes();
+}
+
+private loadsKeynotes() {
+  const url = 'http://localhost:8080/keynotes';
+  this.httpClient.get<Keynote[]>(url).subscribe(keynotes => this.keynotes = keynotes);
+}
 
 }
