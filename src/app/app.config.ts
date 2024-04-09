164be4046c346d25c074787124b0bd25be5be9d0
Angel Martinez <angelmaria75@gmail.com>
2024-04-09 17:00:47 +0200

    añado el povideHttpClient global en app.config.ts y eliminamos el HttpClientModule de varios forms, excepto login y register

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,17 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { jwtInterceptor } from './authentication/jwt.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+  // configuración de HttpClient con interceptor JWT:
+  // Ya no hace falta import de HttpClientModule en los components
+  provideHttpClient(withInterceptors([jwtInterceptor]))
+  
+  ]
+
+
 };
