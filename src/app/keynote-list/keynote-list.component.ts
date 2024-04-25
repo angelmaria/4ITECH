d896e23bd5cf889cff6b5876d962c731b3734604
Angel Martinez <angelmaria75@gmail.com>
2024-04-25 14:12:50 +0200

    primera fase en hacer funcional el buscador de charlas del keynote-list, por ahora solo las identifica. Corrección de algún tipo en el home

--- src/app/keynote-list/keynote-list.component.ts
+++ src/app/keynote-list/keynote-list.component.ts
@@ -2,18 +2,42 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Keynote } from '../models/keynote.model';
-import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
-import { DatePipe } from '@angular/common';
+import { NgbAlertModule, NgbTypeaheadModule } from '@ng-bootstrap/ng-bootstrap';
+import { DatePipe, JsonPipe } from '@angular/common';
 import { AuthenticationService } from '../authentication/authentication.service';
+import { Observable, OperatorFunction, debounceTime, distinctUntilChanged, map } from 'rxjs';
+import { FormsModule } from '@angular/forms';
+
+const charlas = [
+	'Inteligencia Artificial',
+	'Realidad Virtual',
+	'Experiencia de Usuario (UX)',
+	'Ciberseguridad',
+	'Cloud Computing',
+	'Blockchain',
+	'Bid Data',
+	'Internet de las cosas (IoT)',
+];
 
 @Component({
   selector: 'app-keynote-list',
   standalone: true,
-  imports: [RouterLink, NgbAlertModule, DatePipe],
+  imports: [RouterLink, NgbAlertModule, DatePipe, NgbTypeaheadModule, FormsModule, JsonPipe],
   templateUrl: './keynote-list.component.html',
   styleUrl: './keynote-list.component.css'
 })
 export class KeynoteListComponent implements OnInit {
+
+  model: any;
+
+	search: OperatorFunction<string, readonly string[]> = (text$: Observable<string>) =>
+		text$.pipe(
+			debounceTime(200),
+			distinctUntilChanged(),
+			map((term) =>
+				term.length < 2 ? [] : charlas.filter((v) => v.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10),
+			),
+		);
   
   keynotes: Keynote[] = [];
   showDeleteKeynoteMessage: boolean = false;
