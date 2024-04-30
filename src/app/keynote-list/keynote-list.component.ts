bf47ee59bdc18c3ea7000c1e896630f17a221e5e
Angel Martinez <angelmaria75@gmail.com>
2024-04-30 17:50:04 +0200

    cambios de Alan en clase: funcionalidad del buscador de charlas keynote-list, y ticket-list bucle for para los 3 tipos de tickets

--- src/app/keynote-list/keynote-list.component.ts
+++ src/app/keynote-list/keynote-list.component.ts
@@ -1,11 +1,11 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, Injectable, OnInit, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Keynote } from '../models/keynote.model';
-import { NgbAlertModule, NgbTypeaheadModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbAlertModule, NgbTypeaheadModule, NgbTypeaheadSelectItemEvent } from '@ng-bootstrap/ng-bootstrap';
 import { DatePipe, JsonPipe } from '@angular/common';
 import { AuthenticationService } from '../authentication/authentication.service';
-import { Observable, OperatorFunction, debounceTime, distinctUntilChanged, map } from 'rxjs';
+import { Observable, OperatorFunction, catchError, debounceTime, distinctUntilChanged, map, of, switchMap, tap } from 'rxjs';
 import { FormsModule } from '@angular/forms';
 import { DomSanitizer } from '@angular/platform-browser';
 
@@ -20,25 +20,55 @@ const charlas = [
 	'Internet de las cosas (IoT)',
 ];
 
+@Injectable()
+export class KeynoteSearchService {
+	private http = inject(HttpClient);
+
+	search(term: string) {
+		if (term === '') {
+			return of([]);
+		}
+
+		return this.http
+			.get<[any, string[]]>(`http://localhost:8080/keynotes/filter-by-title-arr/${term}`)
+			.pipe(map((response) => {
+        console.log(response);
+        return response;
+        
+      }));
+	}
+  findAllFilterByTitle(term: string) {
+    return this.http.get<Keynote[]>(`http://localhost:8080/keynotes/filter-by-title/${term}`);
+  }
+}
+
 @Component({
   selector: 'app-keynote-list',
   standalone: true,
   imports: [RouterLink, NgbAlertModule, DatePipe, NgbTypeaheadModule, FormsModule, JsonPipe],
   templateUrl: './keynote-list.component.html',
-  styleUrl: './keynote-list.component.css'
+  styleUrl: './keynote-list.component.css',
+  providers: [KeynoteSearchService]
 })
 export class KeynoteListComponent implements OnInit {
 
-  model: any;
 
-	search: OperatorFunction<string, readonly string[]> = (text$: Observable<string>) =>
-		text$.pipe(
-			debounceTime(200),
-			distinctUntilChanged(),
-			map((term) =>
-				term.length < 2 ? [] : charlas.filter((v) => v.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10),
-			),
-		);
+  // model: string = '';
+
+	// search: OperatorFunction<string, readonly string[]> = (text$: Observable<string>) =>
+	// 	text$.pipe(
+	// 		debounceTime(200),
+	// 		distinctUntilChanged(),
+	// 		map((term) =>
+	// 			term.length < 2 ? [] : charlas.filter((v) => v.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10),
+	// 		),
+	// 	);
+
+  model: any;
+	searching = false;
+	searchFailed = false;
+
+
   
   keynotes: Keynote[] = [];
   showDeleteKeynoteMessage: boolean = false;
@@ -49,7 +79,8 @@ export class KeynoteListComponent implements OnInit {
   constructor(
     private httpClient: HttpClient,
     protected sanitizer: DomSanitizer,
-    private authService: AuthenticationService) {
+    private authService: AuthenticationService,
+  private searchService: KeynoteSearchService) {
       this.authService.isAdmin.subscribe(isAdmin => this.isAdmin = isAdmin);
       this.authService.isLoggedIn.subscribe(isLoggedIn => this.isLoggedIn = isLoggedIn);
     }
@@ -58,6 +89,31 @@ export class KeynoteListComponent implements OnInit {
     this.loadsKeynotes();
   }
 
+	search: OperatorFunction<string, readonly string[]> = (text$: Observable<string>) =>
+		text$.pipe(
+			debounceTime(300),
+			distinctUntilChanged(),
+			tap(() => (this.searching = true)),
+			switchMap((term) => 
+				{
+          console.log("searching term");
+          if(!term || term.length === 0)
+            this.loadsKeynotes();
+            
+          return this.searchService.search(term).pipe()
+        },
+			),
+			tap(() => (this.searching = false)),
+		);
+
+    onSelectTitle(event: NgbTypeaheadSelectItemEvent<any>) {
+      console.log(event);
+      
+        this.searchService.findAllFilterByTitle(event.item).subscribe((results) => {
+          this.keynotes = results;
+        });
+      }
+    
   delete(keynote: Keynote) {
     // traer una lista de keynotes del backend: crea y ejecuta una petición HTTP contra un controlador Backend
     const url = 'http://localhost:8080/keynotes/' + keynote.id;
